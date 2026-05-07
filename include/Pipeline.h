1d5831cb643d43b08bea48f3cfce336a7437ebcf
hal <ksrghal@gmail.com>
2026-05-07 12:29:30 +0900

    Rename package

--- include/Pipeline.h
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2026 nanami-v2
-#pragma once
-
-#include "./Pipeline/ExecutionLine.h"
-#include "./Pipeline/ExecutionUnit.h"
-#include "./Pipeline/Gate.h"
